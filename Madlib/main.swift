8610d10f47c9635a3f4f3a995249bad63c414e6d
Guest User <coder@mainlab22.local>
2016-01-08 11:20:40 -0800

    madlib1

--- Madlib/main.swift
+++ Madlib/main.swift
@@ -18,26 +18,28 @@ func input() -> String {
 
 
 print("when a user visits our site for the first time, he or she should immediately see that we are ")
-
 print("adjetive")
 let n = input()
-
 print(" and ")
-
 print("adjective")
 let n1 = input()
-
-print(" When that user digs deeper into the site, he or she will also notice that we are a ")
+print("  When that user digs deeper into the site, he or she will also notice that we are ")
 print("noun")
 let n2 = input()
-print(",")
+print(" and ")
 print("adjective")
 let n3 = input()
-
-
-print(", and ")
+print(".")
+print("When a user shares our content with peers and colleagues, he or she will tell them that we are a")
 print("adjective")
 let n4 = input()
+print(", ")
+print("adjective")
+let n5 = input()
+print(", and ")
+print("adjective")
+let n6 = input()
 print("organization.")
 
-print("when a user visits our site for the first time, he or she should immediately see that we are  \(n) and \(n1)When that user digs deeper into the site, he or she will also notice that we are a \(n2), and \(n3) organization.)")
+
+print("when a user visits our site for the first time, he or she should immediately see that we are  \(n) and \(n1). When that user digs deeper into the site, he or she will also notice \(n2) and \(n3). When a user shares our content with peers and colleagues, he or she will tell them that we are a  \(n4), \(n5), and \(n6) organization.)")
