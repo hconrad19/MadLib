67fb1e42006d2d689fd420c773fccc811210bd22
Guest User <coder@mainlab22.local>
2016-01-12 11:31:45 -0800

    sdf

--- Madlib/main.swift
+++ Madlib/main.swift
@@ -30,7 +30,7 @@ while playCheck {
 }
 
 
-
+//Bryces madlib
 func website(){
 
 
@@ -75,35 +75,49 @@ func website(){
     
             print("when a user visits our site for the first time, he or she should immediately see that we are  \(n) and \(n1). When that user digs deeper into the site, he or she will also notice \(n2) and \(n3). When a user shares our content with peers and colleagues, he or she will tell them that we are a  \(n4), \(n5), and \(n6) organization.)")
 }
-
+//Bryces madlib
         func OneDay(){
-        }
+
         print("One Day ")
         print("Proper Noun")
+            
         let b = input()
+            
         print(" was ")
         print("Verb-ING")
+            
         let b1 = input()
+            
         print(" to the ")
         print("noun")
+            
         let b2 = input()
+            
         print(". On the way, ")
         print("pronoun")
+            
         let b3 = input()
+            
         print(" Saw a ")
         print("noun")
+            
         let b4 = input()
+            
         print(" This was a surprise so ")
         print("pronoun")
+            
         let b5 = input()
+            
         print("verb")
+            
         let b6 = input()
+            
         print(" Quickly.")
         
         
         print("One day \(b) was \(b1) to the \(b2). On the way, \(b3) saw a \(b4)  This was a surprice so \(b5) \(b6) Quickly.")
 
-
+}
 
 
 
